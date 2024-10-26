f14bc02121796702c4e60b7ef85a6c33b932c2a2
Anton <fynjn77@gmail.com>
2024-10-26 09:49:06 +0300

    all adaptive, but without animation

--- assets/js/swiper-sliders.js
+++ assets/js/swiper-sliders.js
@@ -1,47 +1,58 @@
 var swiper = new Swiper(".digital-detail__slider", {
-    loop: true,
-     pagination: {
-      el: ".digital-detail__pagination",
-      // type: "fraction",
-
-      type: 'custom',
-      renderCustom: function (swiper, current, total) {
-          return '<span class="digital-detail__pagination-num">'+'0'+current +'</span>' + '<hr/>' + '<span class="digital-detail__pagination-num">'+'0'+total +'</span>' + '<div><span class="' + className + '">' + (index + 1) + '</span></div>';
-          
-      },
-
-      // el: ".digital-detail__pagination-mobile",
-      // clickable: true,
+    loop: false,
+    init: false,
+    pagination: {
+      el: '.digital-detail__pagination-mobile',
+      type: "bullets",
+      clickable: true,
+  },
 
 
-      // renderBullet: function (index, className) {
-      //   return '<span class="' + className + '">' + (index + 1) + '</span>';
-      // }
-    },  
-
-
-      navigation: {
-        nextEl: ".digital-detail__button-next",
-        prevEl: ".digital-detail__button-prev",
-      },
+    navigation: {
+      nextEl: ".digital-detail__button-next",
+      prevEl: ".digital-detail__button-prev",
+    },
     slidesPerView: 2.25,
     spaceBetween: 80,
     breakpoints: {
 
-      768: {
-        slidesPerView: 1.25,
+      769: {
+        slidesPerView: 2.25,
         spaceBetween: 60,
+        // pagination: {
+        //   el: ".digital-detail__pagination",
+        //   // type: "fraction",
+    
+        //   type: 'custom',
+        //   renderCustom: function (swiper, current, total) {
+        //       return '<span class="digital-detail__pagination-num">'+'0'+current +'</span>' + '<hr/>' + '<span class="digital-detail__pagination-num">'+'0'+total +'</span>';
+              
+        //   },
+        // }, 
       },
       320: {
-        pagination:{
-          el: ".digital-detail__pagination-mobile",
-          clickable: true,
-        },
         slidesPerView: 1.25,
         spaceBetween: 10,
+   pagination: {
+      el: '.digital-detail__pagination-mobile',
+      type: "bullets",
+      clickable: true,
+  },
       },
     }
 
     // centeredSlides: true,
 
   });
+
+
+  swiper.on("slideChange afterInit slidesLengthChange", function () {
+		let currentSlide = this.activeIndex + 1;
+		document.querySelector('.digital-detail__pagination').innerHTML = `
+    <span class="digital-detail__pagination-num">0${currentSlide}</span><hr/>
+<span class="digital-detail__pagination-num"> 0${this.slides.length}</span>`;
+
+});
+
+swiper.init();
+
